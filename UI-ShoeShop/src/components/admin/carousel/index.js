1a80d6af84382b084c617bb4dd3f0e7afc179fd9
Thu Tinh <minhtri2041998@gmail.com>
2019-10-08 00:48:38 +0700

    add carousel

---
+++ UI-ShoeShop/src/components/admin/carousel/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Carousel } from 'antd';
+import './style.css';
+
+
+
+
+function Carousels() {
+  return (
+    <Carousel autoplay className = "custom-carousel">
+      <div className = "custom-carousel">
+        <h3>1dgj f</h3>
+      </div>
+      <div className = "custom-carousel">
+        <h3>2</h3>
+      </div>
+      <div className = "custom-carousel">
+        <h3>3</h3>
+      </div>
+      <div className = "custom-carousel">
+        <h3>4</h3>
+      </div>
+    </Carousel> 
+  );
+}
+
+export default Carousels;
