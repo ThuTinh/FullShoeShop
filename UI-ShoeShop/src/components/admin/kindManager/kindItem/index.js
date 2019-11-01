da4a354047c460a1c6a4a0c19505eea43fe5a7ce
Thu Tinh <minhtri2041998@gmail.com>
2019-11-01 10:27:28 +0700

    something

--- UI-ShoeShop/src/components/admin/kindManager/kindItem/index.js
+++ UI-ShoeShop/src/components/admin/kindManager/kindItem/index.js
@@ -5,7 +5,7 @@ import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { makeStyles } from "@material-ui/core/styles";
 import { atcDeleteCaregoryRequest } from "../../../../actions";
-import {connect} from "react-redux"
+import { connect } from "react-redux";
 const usestyles = makeStyles(theme => ({
   icon: {
     color: "#512C62",
@@ -19,18 +19,17 @@ const usestyles = makeStyles(theme => ({
 function KindItem(props) {
   const classes = usestyles();
   const category = props.category;
-  const numberCategorySub = category.children.length; 
-  const remove = (id) =>{
-    console.log("id ne", id)
+  const numberCategorySub = category.children.length;
+  const remove = id => {
+    console.log("id ne", id);
     props.deleteCategory(id);
-
-  }
+  };
   const renderContent = () => {
-    console.log("render: ", )
+    console.log("render: ");
     var result = [];
     if (numberCategorySub === 0) {
       result[0] = (
-        <TableRow key = {0}>
+        <TableRow key={0}>
           <TableCell rowSpan={4} align="center">
             {category.name}
           </TableCell>
@@ -43,46 +42,54 @@ function KindItem(props) {
       );
     } else {
       result[0] = (
-        <TableRow key = {0}>
+        <TableRow key={0}>
           <TableCell rowSpan={numberCategorySub} align="center">
             {category.name}
           </TableCell>
           <TableCell align="center">{category.children[0].name}</TableCell>
           <TableCell>
             <EditIcon className={classes.icon}> </EditIcon>{" "}
-            <DeleteIcon className={classes.icon} onClick = {()=>remove(category.children[0]._id)}> </DeleteIcon>
+            <DeleteIcon
+              className={classes.icon}
+              onClick={() => remove(category.children[0]._id)}
+            >
+              {" "}
+            </DeleteIcon>
           </TableCell>
         </TableRow>
       );
 
       for (var i = 1; i < numberCategorySub; i++) {
         result[i] = (
-          <TableRow key = {i}>
+          <TableRow key={i}>
             <TableCell align="center">{category.children[i].name}</TableCell>
             <TableCell>
               <EditIcon className={classes.icon}> </EditIcon>{" "}
-              <DeleteIcon className={classes.icon} onClick = {()=>remove(category.children[i]._id)}> </DeleteIcon>
+              <DeleteIcon
+                className={classes.icon}
+                onClick={() => remove(category.children[i]._id)}
+              >
+                {" "}
+              </DeleteIcon>
             </TableCell>
           </TableRow>
         );
       }
-
     }
-    console.log("render: ", numberCategorySub)
+    console.log("render: ", numberCategorySub);
     return result;
   };
-  return (
-    <>
-     {renderContent()}
-    </>
-  );
+  return <>{renderContent()}</>;
 }
 
 const dispatchMapToProps = (dispatch, props) => {
   return {
-    deleteCategory: (id) => {
+    deleteCategory: id => {
       dispatch(atcDeleteCaregoryRequest(id));
     }
   };
 };
-export default connect(null, dispatchMapToProps) (KindItem);
+export default connect(
+  null,
+  dispatchMapToProps
+)(KindItem);
